4567d155f9b265a9cf64e6e52f707e48052d90f9
FranciscoRosal <ros18676@uvg.edu.gt>
2021-04-07 21:56:15 -0600

    feat: add auth on firebase

--- users/views.py
+++ users/views.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import action
 from users.serializers import UserSerializer
 from permissions.services import APIPermissionClassFactory
 
+from firebase_admin import auth
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -37,6 +39,17 @@ class UserViewSet(viewsets.ModelViewSet):
         )
         usuario.set_password(request.data['password'])
         usuario.save()
+
+        # Firebase user
+        # https://firebase.google.com/docs/auth/admin/manage-users#python_4
+        user = auth.create_user(
+            email=request.data['email'],
+            email_verified=True,
+            password=request.data['password'],
+            display_name=request.data['username'],
+            photo_url='http://www.example.com/12345678/photo.png',
+            disabled=False
+        )
 
         return Response({
             'status':'Ok'
