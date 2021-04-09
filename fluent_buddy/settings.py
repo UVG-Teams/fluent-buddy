2b9cfbe67936ec00e4adc99d0aa429a1273108a1
Marco <marcofuentes1607@gmail.com>
2021-04-08 19:31:32 -0600

    Funcionalidad de email implementada

--- fluent_buddy/settings.py
+++ fluent_buddy/settings.py
@@ -31,6 +31,14 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
+# Email stuff
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fluentbuddyinfo@gmail.com'
+EMAIL_HOST_PASSWORD = 'PatoLuca5'
 
 # Application definition
 
--- users/views.py
+++ users/views.py
@@ -6,6 +6,32 @@ from rest_framework.decorators import action
 
 from users.serializers import UserSerializer
 from permissions.services import APIPermissionClassFactory
+from users.services import sendMail
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def index(request):
+    message = 'El correo fue enviado con éxito'
+    messageError = 'El correo <strong>no</strong> fue enviado con éxito'
+    try:
+        sendMail(request)
+        return render(
+        request,
+        'signup/response.html',
+            {
+                'message': message,
+                'direccion': request.POST['email']
+            }
+        )
+    except:
+        return render(
+        request,
+        'signup/resopnseError.html',
+            {
+                'message': message,
+                'direccion': request.POST['email']
+            }
+        )
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -37,7 +63,7 @@ class UserViewSet(viewsets.ModelViewSet):
         )
         usuario.set_password(request.data['password'])
         usuario.save()
-
+        sendMail(request)
         return Response({
             'status':'Ok'
         })
