56b8f76473e94c4b8abe1b7e903887fd80fb0d94
FranciscoRosal <ros18676@uvg.edu.gt>
2021-04-09 00:01:47 -0600

    feat: connection signup with firebase

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework_jwt.settings import api_settings
 
 from users.serializers import UserSerializer
 from permissions.services import APIPermissionClassFactory
@@ -39,42 +40,39 @@ class UserViewSet(viewsets.ModelViewSet):
         if request.data['type'] == 'normal':
             email = request.data['email']
             display_name = request.data['username']
-            photo = None
             password = request.data['password']
+
+            # Firebase user
+            # https://firebase.google.com/docs/auth/admin/manage-users#python_4
+            firebase_user = auth.create_user(
+                email = email,
+                email_verified = True,
+                password = password,
+                display_name = display_name,
+                photo_url = None,
+                disabled = False
+            )
         else:
             email = request.data['user']['email']
             display_name = request.data['user']['name']
-            photo = request.data['user']['picture']['data']['url']
+            # photo = request.data['user']['picture']['data']['url']
             password = hmac.new(
                 msg = bytes(request.data['user']['id'], 'utf-8'),
                 key = bytes(request.data['user']['id'], 'utf-8'),
                 digestmod = hashlib.sha256
             ).hexdigest()
 
-        print("email: ", email)
-        print("password: ", password)
-        print("display_name: ", display_name)
-        print("photo: ", photo)
-
-        usuario = User(
+        user = User(
             username = email,
             email = email,
         )
 
-        usuario.set_password(password)
-        usuario.save()
+        user.set_password(password)
+        user.save()
 
-        # Firebase user
-        # https://firebase.google.com/docs/auth/admin/manage-users#python_4
-        # firebase_user = auth.create_user(
-        #     email = email,
-        #     email_verified = True,
-        #     password = password,
-        #     display_name = display_name,
-        #     photo_url = photo,
-        #     disabled = False
-        # )
+        payload = api_settings.JWT_PAYLOAD_HANDLER(user)
+        token = api_settings.JWT_ENCODE_HANDLER(payload)
 
         return Response({
-            'status':'Ok'
+            'token': token
         })
